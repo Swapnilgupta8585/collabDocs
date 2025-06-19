238e5240fea3e48af33a4cbee0a07418f9a48fb5
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-06-19 10:08:36 +0530

    add users Table

---
+++ backend/sql/schema/001_users.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    full_name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
