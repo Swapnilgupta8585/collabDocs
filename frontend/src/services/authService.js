eed6d1e14ca1a486527620fb184e0b97995641f4
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-06-19 10:42:36 +0530

    add refresh authentication related services here

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import useAuthStore from "../store/authStore";
+
+const API_URL = "http://localhost:5000/api";
+
+const authService = {
+    async register(full_name, email, password) {
+        try {
+            const response = await axios.post(`${API_URL}/users`, {
+                full_name,
+                email,
+                password,
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+            return response.data;
+        } catch (error) {
+            throw new Error(
+                error.response?.data?.error || error.message || "Something went wrong"
+            );
+        }
+    },
+
+    async login(email, password) {
+        try {
+            const response = await axios.post(`${API_URL}/login`, {
+                email,
+                password,
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            return response.data
+        } catch (error) {
+            console.error("Login error:", error);
+            throw new Error(
+                error.response?.data?.error || error.message || "Something went wrong"
+            );
+        }
+    },
+
+    logout() {
+        try {
+            useAuthStore.getState().clearAuth();
+            return true
+        } catch (error) {
+            console.error("Logout error:", error);
+            return false
+        }
+    }
+};
+
+export default authService;
