82c132eaea6ee971da8097954bf9e0019453e5be
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-06-26 12:07:36 +0530

    configured cors for local development

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CORS() func(http.Handler) http.Handler {
 	return cors.New(cors.Options{
-		AllowedOrigins: []string{"https://collabdoc.xyz","https://www.collabdoc.xyz"},
+		AllowedOrigins: []string{"https://collabdoc.xyz","https://www.collabdoc.xyz", "https://collab-docs-nine.vercel.app", "http://localhost:5173"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
